eef025309bb2c45698bb7aebbeda44edd702f3c4
Panu Horsmalahti <panu.horsmalahti@iki.fi>
2014-06-29 12:26:43 +0300

    Add LIBMTP_Set_Debug and debug constants

--- index.js
+++ index.js
@@ -9,7 +9,17 @@
 var ffi = require('ffi');
 
 var libmtp = new ffi.Library('libmtp', {
-    'LIBMTP_Init': [ 'void', [ ] ]
+    // 'Function name': [ 'return_type', [ 'param1', 'param2'] ]
+    'LIBMTP_Init':      [ 'void', [ ] ],
+    'LIBMTP_Set_Debug': [ 'void', [ 'int' ] ]
 });
+
+// Constants
+libmtp.LIBMTP_DEBUG_NONE = 0x00;
+libmtp.LIBMTP_DEBUG_PTP = 0x01;
+libmtp.LIBMTP_DEBUG_PLST = 0x02;
+libmtp.LIBMTP_DEBUG_USB = 0x04;
+libmtp.LIBMTP_DEBUG_DATA = 0x08;
+libmtp.LIBMTP_DEBUG_ALL = 0xFF;
 
 module.exports = libmtp;
--- test/detect.js
+++ test/detect.js
@@ -2,3 +2,6 @@ var mtp = require('../index.js');
 
 // Init
 mtp.LIBMTP_Init();
+
+// Set debug
+mtp.LIBMTP_Set_Debug(mtp.LIBMTP_DEBUG_PTP | mtp.LIBMTP_DEBUG_DATA);
